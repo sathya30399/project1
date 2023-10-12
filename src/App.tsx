5f3887dd332de73a8c137e8c7177fc3cbc781906
sathya30399 <sathya30399@gmail.com>
2023-10-12 12:27:32 +0530

    card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import Form from './components/Form/Form'
+import { useState } from 'react'
+import Btn from "./components/card/Btn"
+import Like from "./components/card/Like"
+import Btun from './components/card/Btun'
+import "./App.css"
 
-const App = () => {
-  return (
-    <Form/>
-  )
+const App =()=>{
+  
+  return (<>
+    <div className="App">
+      <Btn btnlab="Register"  />
+      <Like/>
+
+          
+     
+    
+    </div>
+    </>)
 }
 
 export default App
